f185fb1d662bec1f53bde8c96d6229364738809c
Shay Abraham <shaykos@gmail.com>
2026-03-11 10:21:26 +0200

    הוספת דוגמה להדפסת לוח הכפל באמצעות פונקציה

--- Fullstack/Functions/scripts/functions.js
+++ Fullstack/Functions/scripts/functions.js
@@ -12,3 +12,40 @@ export function showAlert(msg) {
 export function showAlert2(msg = "Hello World") {
     alert(`your massage: ${msg}`);
 }
+
+// פונקציה המקבלת 4 מספרים ומחזירה את הממוצע
+export function CalcAverage(n1, n2, n3, n4) {
+    let avg = (n1 + n2 + n3 + n4) / 4;
+    return avg;
+}
+
+//שנשלח limit פונקציה המדפיסה את לוח הכפל בהתאם לפרמטר 
+// כברירת מחדל היא תדפיס את לוח הכפל של 10
+export function PrintMultiTable(limit = 10) {
+
+    // שלב 1 - תופסים את האלמנט בו יוצג לוח הכפל
+    let div = document.querySelector('#answer');
+
+    // שלב 2 - מתחילים ליצור את התבנית של לוח הכפל
+    let table = '<table>';
+
+    // שלב 3 - למלא את לוח הכפל
+    for (let row = 1; row <= limit; row++) {
+        // שלב 3.1 - יצירת שורה 
+        table += '<tr>';
+
+        // שלב 3.2 - יוצרים את התאים בכל שורה
+        for (let col = 1; col <= limit; col++) {
+            table += `<td>${row * col}</td>`;
+        }
+
+        // שלב 3.3 - סוגרים את השורה
+        table += '</tr>';
+    }
+
+    // שלב 3.4 - סוגרים את הטבלה
+    table += '</table>';
+
+    // div שלב 4 - הזרקת הטבלה לתוך ה 
+    div.innerHTML += table;
+}
--- Fullstack/Functions/scripts/index.js
+++ Fullstack/Functions/scripts/index.js
@@ -1,8 +1,21 @@
 // מייבאים את הפונקציה מהקובץ שבו כתבנו אותה
-import { showAlert, showAlert2 } from "./functions.js";
+import { CalcAverage, PrintMultiTable, showAlert, showAlert2 } from "./functions.js";
 
 // כאן אנחנו משתמשים בה
-showAlert("הודעה 1");
-showAlert("הודעה 2");
-showAlert2();
-showAlert2("הודעה 3");
+//showAlert("הודעה 1");
+//showAlert("הודעה 2");
+//showAlert2();
+//showAlert2("הודעה 3");
+
+// קליטת המספרים
+// let a = Number(prompt("הזן מספר ראשון"));
+// let b = Number(prompt("הזן מספר שני"));
+// let c = Number(prompt("הזן מספר שלישי"));
+// let d = Number(prompt("הזן מספר רביעי"));
+
+// average הפעלת הפונקציה ושמירה של הערך המוחזר במשתנה 
+// let average = CalcAverage(a, b, c, d);
+// alert(average);
+
+PrintMultiTable();
+PrintMultiTable(20);
