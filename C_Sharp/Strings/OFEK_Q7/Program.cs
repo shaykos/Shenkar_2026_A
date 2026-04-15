bac5f533ec30e9f4128bdcb1377f54c6604cc3ab
Shay Abraham <shaykos@gmail.com>
2026-04-15 11:18:33 +0300

    Add OFEK_Q7 and Q7 projects with initial implementations and functionality

---
+++ C_Sharp/Strings/OFEK_Q7/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+
+        string reversedTwoLetters = FirstLettersSwapper(name);
+
+        Console.WriteLine(reversedTwoLetters);
+    }
+
+    static string FirstLettersSwapper(string name)
+    {
+        string temp = name;
+
+        string firstLetter = temp[0].ToString();
+        string secondLetter = temp[1].ToString();
+
+        Console.WriteLine($"{firstLetter} {secondLetter}");
+
+        string swappedName = ""; //temp.Replace(firstLetter, secondLetter);
+        swappedName += secondLetter;
+        swappedName += firstLetter;
+        swappedName += temp.Substring(2);
+
+        return swappedName;
+    }
+}
---
+++ C_Sharp/Strings/Q7/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        string reversedTwoLetters = FirstLettersSwapper(name);
+        Console.WriteLine(reversedTwoLetters);
+    }
+
+    static string FirstLettersSwapper(string name)
+    {
+        // This method swaps the first two letters of the input string and returns the modified string.
+        string str = name[1].ToString() + name[0].ToString();
+
+        // We start the loop from index 2 because we have already swapped the first two letters.
+        for (int i = 2; i < name.Length; i++)
+        {
+            str += name[i];
+        }
+        return str;
+    }
+}
