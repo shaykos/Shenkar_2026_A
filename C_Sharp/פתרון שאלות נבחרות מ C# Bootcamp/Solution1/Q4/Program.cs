d72e1bbeddbca59d9b3d0f0b97fa8e1fa6a91d1c
Shay Abraham <shaykos@gmail.com>
2026-04-28 09:55:38 +0300

    פתרון שאלה 4

---
+++ C_Sharp/פתרון שאלות נבחרות מ C# Bootcamp/Solution1/Q4/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] colors;
+
+            Console.Write("How many players: ");
+            int players = int.Parse(Console.ReadLine());
+
+            // יוצרים את מערך הצבעים
+            colors = new string[players];
+
+            //מפעילים פונקציה שמאפשרת לשמחקנים לבחור את הצבע שלהם
+            GetColorsFromPlayers(colors);
+
+            //פונקציה שמתחילה את המשחק
+            StartGame(colors);
+        }
+
+        static void GetColorsFromPlayers(string[] colors)
+        {
+            for (int i = 0; i < colors.Length; i++)
+            {
+                Console.Write($"Player #{i + 1}: ");
+                colors[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Done!");
+        }
+
+        static void StartGame(string[] colors)
+        {
+            //מערך ששומר את כמות ההגרלות שיצאו לכל צבע
+            int[] counts = new int[colors.Length];
+
+            string input;
+            int index;
+            Random rnd = new Random();
+
+            do { 
+                index = rnd.Next(0, colors.Length);
+                Console.WriteLine(colors[index]);
+                counts[index]++;
+                Console.Write("to stop enter 'q': ");
+                input = Console.ReadLine();
+            } while (input != "q");
+
+            //נדפיס את תוצאות ההגרלה
+            for (int i = 0; i < counts.Length; i++)
+            {
+                Console.WriteLine($"{colors[i]}: {counts[i]}");
+            }
+        }
+    }
+}
