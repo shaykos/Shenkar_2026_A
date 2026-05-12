87caab58c4324502ff4479f9dc227ac6b7dabd30
Shay Abraham <shaykos@gmail.com>
2026-05-12 15:41:32 +0300

    Object cs

--- C_Sharp/05 - OOP/02 - חזרה וסיכום/ConsoleApp1/Person.cs
+++ C_Sharp/05 - OOP/02 - חזרה וסיכום/ConsoleApp1/Person.cs
@@ -8,5 +8,22 @@ namespace ConsoleApp1
 {
     internal class Person
     {
+        public string eyeColor;
+        public string hairColor;
+        public int age;
+
+        public Person(string eyeColor, string hairColor, int age)
+        {
+            
+        }
+        public Person()
+        {
+
+        }
+        public Person(Person p) {
+            this.eyeColor = p.eyeColor;
+            this.hairColor = p.hairColor;
+            this.age = p.age;
+        }
+        }
     }
-}
--- C_Sharp/05 - OOP/02 - חזרה וסיכום/ConsoleApp1/Program.cs
+++ C_Sharp/05 - OOP/02 - חזרה וסיכום/ConsoleApp1/Program.cs
@@ -4,12 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
 
-            Destination destination = new Destination("Paris", 199.99, 5);
-            Console.Write(destination); // ToString אוטומטית מפעיל את הפונקציה
+            //Destination destination = new Destination("Paris", 199.99, 5);
+            //Console.Write(destination); // ToString אוטומטית מפעיל את הפונקציה
 
-            Console.WriteLine();
+            //Console.WriteLine();
+
+            Person yam = new Person("Blue", "Brown", 25);
+            Person noam = new Person("Green", "Blonde", 30);
+            Person adam = new Person(noam);
+
+            int[] arr = new int[] { 1, 2, 3, 4, 5 };
+            Person[] persons = new Person[] { yam, noam, adam };
+
+            foreach (Person p in persons)
+            {
+                Console.WriteLine(p.age);
+            }
+            //-------------------------------------------------------
+            for(int i=0; i < persons.Length; i++)
+            {
+                Console.WriteLine(persons[i].age);
+            }
+
         }
     }
 }
